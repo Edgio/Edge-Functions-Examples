9f5bbbb147387e6c534ea031e69162053cfb514e
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2023-07-05 22:25:58 -0400

    Initialize with Edgio

---
+++ routes.js
@@ -0,0 +1,17 @@
+// This file was added by edgio init.
+// You should commit this file to source control.
+import { Router, edgioRoutes } from '@edgio/core'
+
+export default new Router()
+  // Here is an example where we cache api/* at the edge but prevent caching in the browser
+  // .match('/api/:path*', {
+  //   caching: {
+  //     max_age: '1d',
+  //     stale_while_revalidate: '1h',
+  //     bypass_client_cache: true,
+  //     service_worker_max_age: '1d',
+  //   },
+  // })
+
+  // plugin enabling basic Edgio functionality
+  .use(edgioRoutes)
