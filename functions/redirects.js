b1750f181c6b232d706f10b699d29443c5da7984
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2023-10-05 22:47:28 -0400

    Simplify redirect function

--- functions/redirects.js
+++ functions/redirects.js
@@ -4,9 +4,9 @@ import { URL } from 'whatwg-url'
 export function handleHttpRequest(request, context) {
   const url = new URL(request.url)
 
-  if (redirects[url.pathname]) {
-    return new Response("", { status: 301, headers: { location: redirects[url.pathname] } });
+  if (!redirects[url.pathname]) {
+    return new Response("Not found", { status: 404 });
   }
 
-  return fetch(request)
+  return new Response("", { status: 301, headers: { location: redirects[url.pathname] } });
 }
